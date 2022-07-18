6225f3d55eeb5da1722791a3f69e8a2c4541751b
Taiga Nakayama <t_nakayama@fanfare-kk.com>
2022-07-18 10:02:59 +0900

    feat: added a sample component

---
+++ components/SampleComponent.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <!-- サンプルのコンポーネントです -->
+  </div>
+</template>
+
+<script lang="ts">
+import Vue, { PropType } from 'vue';
+
+export default Vue.extend({
+  name: 'SampleComponent',
+  props: {
+    sample: {
+      type: String as PropType<string | undefined>,
+      required: false,
+      default: undefined,
+    },
+  },
+  methods: {
+    // sample
+  },
+});
+</script>
+
+<style scoped lang="scss">
+// sample
+</style>
